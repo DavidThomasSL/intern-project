30a087147b1d514f7f7d934f0ffc70b6472fc604
James Hughes <jh13293@my.bristol.ac.uk>
2015-07-14 10:25:30 +0100

    formatting files

--- client/js/client.js
+++ client/js/client.js
@@ -1,22 +1,22 @@
-
 var messageList = document.getElementById('messages');
 var textBox = document.getElementById('message-box');
 
 var socket = io.connect();
 
-socket.on('message', function (msg) {
-    var newItem = document.createElement('li');
-    newItem.textContent = msg;
-    
-    messageList.appendChild(newItem);
-    
-    messageList.scrollTop = messageList.scrollHeight;
+socket.on('message', function(msg) {
+
+	var newItem = document.createElement('li');
+	newItem.textContent = msg;
+
+	messageList.appendChild(newItem);
+
+	messageList.scrollTop = messageList.scrollHeight;
 });
 
-var send = function () {
-    var text = textBox.value;
-    socket.emit('message', text);
-    textBox.value = '';
-    
-    return false;
+var send = function() {
+	var text = textBox.value;
+	socket.emit('message', text);
+	textBox.value = '';
+
+	return false;
 };
