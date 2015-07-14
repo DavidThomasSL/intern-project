324862ac9ad07c9f0863ff5b0b5e3407127b66b1
unknown <gginghina@SL-WS-026.scottlogic.co.uk>
2015-07-14 14:04:38 +0100

    added persistant sessions for users

--- client/js/client.js
+++ client/js/client.js
@@ -4,19 +4,52 @@ var textBox = document.getElementById('message-box');
 
 var socket = io.connect();
 
-socket.on('message', function (msg) {
-    var newItem = document.createElement('li');
-    newItem.textContent = msg;
-    
-    messageList.appendChild(newItem);
-    
-    messageList.scrollTop = messageList.scrollHeight;
+socket.on('connect', function() {
+
+	//tell the server to register us as a new player or get our old profile
+	socket.emit('join', {token: getCookie("token")});
+
+	socket.on('allocate id', function(msg) {
+		console.log("Setting cookie for new user" + msg.uId);
+		setCookie('token', msg.uId);
+	});
+
+	socket.on('message', function (msg) {
+
+	    var newItem = document.createElement('li');
+	    console.log(msg)
+	    newItem.textContent = msg.text + " " + msg.id;
+
+	    messageList.appendChild(newItem);
+
+	    messageList.scrollTop = messageList.scrollHeight;
+	});
+
 });
+
 
 var send = function () {
     var text = textBox.value;
-    socket.emit('message', text);
+    socket.emit('message', {text: text, id: getCookie("token")});
     textBox.value = '';
-    
+    console.log(getCookie('token'));
     return false;
 };
+
+function setCookie(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (20*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return undefined;
+}
--- server/server.js
+++ server/server.js
@@ -11,35 +11,85 @@ module.exports = function(port, enableLogging) {
   var server = http.createServer(router);
   var io = socketio.listen(server, { log: enableLogging });
 
+  io.set('log level', 1);
+
   router.use(express.static(path.resolve(__dirname, '../client')));
+
   var messages = [];
-  var sockets = [];
+  var users = [];
+  var uId = 0;
 
   io.on('connection', function (socket) {
-      messages.forEach(function (data) {
-        socket.emit('message', data);
-      });
 
-      sockets.push(socket);
+        //create a new user for the connection
+        var user = {};
+        user.socket = socket;
 
-      socket.on('disconnect', function () {
-        sockets.splice(sockets.indexOf(socket), 1);
-      });
+        /*  client requests to join,
+            send the uId to the player
+            if the player already has a session token,
+            they will respond that they have an id,
+            and we retrieve their user information
+            otherwise, create a new player
+        */
+        socket.on('join', function(msg) {
+            if(msg.token !== undefined) {
+                console.log("exsitng player")
+                //this user previosuly connected and has an id
+                var existingId = msg.token.id;
+                user = users.filter( function(otherUser) {
+                    return otherUser.uId = existingId;
+                });
 
-      socket.on('message', function (msg) {
-        var text = String(msg || '');
+                //TODO ERROR CHECK IF NOT FOUND< OR TWO USERS RETURNED
+            }
+            else {
+                //first time this user has joined
+                user.uId = uId;
+                users.push(user);
+                uId++;
+                socket.emit('allocate id', {uId: user.uId});
+            }
+        });
 
-        if (!text)
-          return;
 
-        broadcast('message', text);
-        messages.push(text);
-      });
+        //send all previosu messages to the new user
+        messages.forEach(function (data) {
+            socket.emit('message', data);
+        });
+
+
+        socket.emit('join', function() {
+
+        });
+
+        socket.on('disconnect', function () {
+            users = users.filter( function(user) {
+                return user.socket !== socket;
+            });
+        });
+
+
+        socket.on('message', function (msg) {
+            // var text = String(msg || '');
+            var sendingText = msg.text;
+            var sendingId = msg.id;
+
+            // if (!text)
+            //     return;
+
+            var toSend = {text: sendingText, id: sendingId};
+            console.log(toSend)
+
+            broadcast('message', toSend);
+            messages.push(toSend);
+
+        });
     });
 
   function broadcast(event, data) {
-    sockets.forEach(function (socket) {
-      socket.emit(event, data);
+    users.forEach(function (user) {
+      user.socket.emit(event, data);
     });
   }
 
