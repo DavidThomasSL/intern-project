35a8784ff1ffc5baff0ea1dd956c8b7c714c74d5
James Hughes <jh13293@my.bristol.ac.uk>
2015-07-16 14:17:11 +0100

    added protractor testing to grunt'

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,82 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-mocha-test");
+    grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-selenium-webdriver');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     var testOutputLocation = process.env.CIRCLE_TEST_REPORTS || "test_output";
     var artifactsLocation = "build_artifacts";
     grunt.initConfig({
         jshint: {
-            all: ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "client/**/*.js"],
+            all: ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "client/js/*.js"],
             options: {
                 jshintrc: true
             }
         },
-        mochaTest: {
-            test: {
-                src: ["test/**/*.js"]
+        protractor: {
+            options: {
+                configFile: 'test/conf.js',
+                args: {
+                    params: {
+                        name: 'CLONAGE'
+                    }
+                }
             },
-            ci: {
-                src: ["test/**/*.js"],
+            e2e: {
                 options: {
-                    reporter: "xunit",
-                    captureFile: testOutputLocation + "/mocha/junit.xml",
-                    quiet: true
+                    // Stops Grunt process if a test fails
+                    keepAlive: false
+                }
+            }
+        },
+        express: {
+            options: {
+                port: 8080
+                    // Override defaults here
+            },
+            test: {
+                options: {
+                    script: './server.js'
+                }
+            }
+        },
+        selenium_start: {
+            options: {
+                port: 4444
+            }
+        },
+        watch: {
+            express: {
+                files: ['./server/*.js'],
+                tasks: ['express:dev'],
+                options: {
+                    spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
                 }
             }
         }
+        // mochaTest: {
+        //     test: {
+        //         src: ["test/**/*.js"]
+        //     },
+        //     ci: {
+        //         src: ["test/**/*.js"],
+        //         options: {
+        //             reporter: "xunit",
+        //             captureFile: testOutputLocation + "/mocha/junit.xml",
+        //             quiet: true
+        //         }
+        //     }
+        // }
     });
 
     grunt.registerTask("check", ["jshint"]);
     grunt.registerTask("test", ["check", "mochaTest:test"]);
     grunt.registerTask("ci-test", ["check", "mochaTest:ci"]);
     grunt.registerTask("default", "test");
+    grunt.registerTask('selenium', ['selenium_start']);
+    grunt.registerTask('server', ['express:test', 'watch']);
+    grunt.registerTask('e2e-test', ['selenium_start', 'express:test', 'protractor:e2e']);
 };
