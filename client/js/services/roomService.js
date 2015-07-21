a0468a678b4f06c6d2c6b3c1af08ad0f5bc1f589
Ben Howard <benrhyshoward@gmail.com>
2015-07-21 15:23:25 +0100

    moved services, controllers and config into seperate files

---
+++ client/js/services/roomService.js
@@ -0,0 +1,50 @@
+ClonageApp.service('roomService', ['socket', '$sessionStorage', function(socket, $sessionStorage) {
+
+    var roomId = -1;
+    var usersInRoom = [];
+
+    function createRoom(playerId) {
+        socket.emit("ROOM create", {
+            playerId: playerId
+        });
+
+    }
+
+    function joinRoom(roomId) {
+        socket.emit('ROOM join', {
+            roomId: roomId
+        });
+    }
+
+    function getUsersInRoom() {
+        console.log(usersInRoom);
+        return usersInRoom;
+    }
+
+    function leaveRoom() {
+        socket.emit('ROOM leave', {
+            roomId: roomId
+        });
+    }
+
+    function getRoomId() {
+        return roomId;
+    }
+
+    socket.on("ROOM details", function(data) {
+        roomId = data.roomId;
+        usersInRoom = data.usersInRoom;
+        console.log("users in room, below");
+        console.log(usersInRoom);
+    });
+
+    return {
+        createRoom: createRoom,
+        joinRoom: joinRoom,
+        usersInRoom: usersInRoom,
+        getUsersInRoom: getUsersInRoom,
+        leaveRoom: leaveRoom,
+        getRoomId: getRoomId
+    };
+
+}]);
