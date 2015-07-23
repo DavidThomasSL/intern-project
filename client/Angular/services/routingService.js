2be315237c641fb93f6d631583ca87784714379b
Ben Howard <benrhyshoward@gmail.com>
2015-07-23 15:36:42 +0100

    Changed routing to switch

--- client/Angular/services/routingService.js
+++ client/Angular/services/routingService.js
@@ -3,20 +3,28 @@ ClonageApp.service('RoutingService', ['socket', '$location', function(socket, $l
     socket.on("ROUTING", function(msg) {
         console.log("sending user to room");
 
-        if (msg.location === 'room') {
-            $location.path('/room');
-        } else if (msg.location === 'joining') {
-            $location.path('/joining');
-        } else if(msg.location === 'question') {
-        	$location.path('/question');
-        } else if(msg.location === 'vote') {
-            $location.path('/vote');
-        } else if(msg.location === 'wait') {
-            $location.path('/wait');
-        } else if(msg.location === 'results') {
-            $location.path('/results');
-        } else if(msg.location === 'endGame') {
-            $location.path('/endGame');
+        switch(msg.location){
+            case 'room' :
+                $location.path('/room');
+                break;
+            case 'joining' :
+                $location.path('/joining');
+                break;
+            case 'question' :
+                $location.path('/question');
+                break;
+            case 'vote' :
+                $location.path('/vote');
+                break;
+            case 'wait' :
+                $location.path('/wait');
+                break;
+            case 'results' :
+                $location.path('/results');
+                break;
+            case 'endGame' :
+                $location.path('/endGame');
+                break;
         }
     });
 
