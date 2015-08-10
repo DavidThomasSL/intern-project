a9b5244a057603cead96d8bbefea3e165dbe81e5
Ben Howard <benrhyshoward@gmail.com>
2015-08-10 17:23:06 +0100

    some better button styling when clicking multiple

--- server/GameController.js
+++ server/GameController.js
@@ -45,10 +45,10 @@ module.exports = function(data) {
 
 				//set up each user
 				var cards = JSON.parse(data);
-				blackCardsMaster = cards.blackCards;
-				// blackCardsMaster = cards.blackCards.filter(function(card) {
-				// 	return (card.pick === 3);
-				// });
+				//blackCardsMaster = cards.blackCards;
+				blackCardsMaster = cards.blackCards.filter(function(card) {
+					return (card.pick !== 1);
+				});
 				whiteCardsMaster = cards.whiteCards;
 				blackCardsCurrent = blackCardsMaster.slice(0);
 				whiteCardsCurrent = whiteCardsMaster.slice(0);
