37debb67b21cf44a5da621f10a0c8c787e541d27
Georgiana <ig13289@my.bristol.ac.uk>
2015-08-18 13:56:10 +0100

    added tests for seeing who submitted

--- test/protractor/playingGameSpec.js
+++ test/protractor/playingGameSpec.js
@@ -37,6 +37,21 @@ describe('When playing a game', function() {
 
 	});
 
+	it('can see who submitted an answer', function() {
+
+		expect(firstClonageUser.element.all(by.id('user-panel')).first().getAttribute('class')).toMatch('player-submitted');
+		expect(secondClonageUser.element.all(by.id('user-panel')).first().getAttribute('class')).toMatch('player-submitted');
+
+	});
+
+	it('can see who did not submit an answer yet', function() {
+
+		expect(firstClonageUser.element.all(by.id('user-panel')).last().getAttribute('class')).toMatch('player-not-submitted');
+		expect(secondClonageUser.element.all(by.id('user-panel')).last().getAttribute('class')).toMatch('player-not-submitted');
+
+	});
+
+
 	it('can refresh and stay on waiting page', function() {
 		firstClonageUser.refresh();
 		expect(browser.getCurrentUrl()).toMatch(/\/wait/);
