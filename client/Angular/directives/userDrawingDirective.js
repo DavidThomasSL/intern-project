89b4efb3a2bea6161dc57f5bf0cf88b1f38d7180
James Hughes <jh13293@my.bristol.ac.uk>
2015-08-24 09:38:52 +0100

    use nice material colours for drawings

--- client/Angular/directives/userDrawingDirective.js
+++ client/Angular/directives/userDrawingDirective.js
@@ -35,7 +35,7 @@ ClonageApp.directive('userDrawing', function() {
 				isDrawingMode: true
 			});
 
-			canvas.freeDrawingBrush.color = rainbow(NUMCOLORS, rand);
+			canvas.freeDrawingBrush.color = getRandomColor();
 			canvas.freeDrawingBrush.width = 7;
 
 			//Add instructional text
@@ -64,50 +64,14 @@ ClonageApp.directive('userDrawing', function() {
 				return canvas.toDatalessJSON();
 			};
 
-			// This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
-			// Adam Cole, 2011-Sept-14
-			// HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-			function rainbow(numOfSteps, step) {
+			function getRandomColor() {
 
-				var r, g, b;
-				var h = step / numOfSteps;
-				var i = ~~(h * 6);
-				var f = h * 6 - i;
-				var q = 1 - f;
-				switch (i % 6) {
-					case 0:
-						r = 1;
-						g = f;
-						b = 0;
-						break;
-					case 1:
-						r = q;
-						g = 1;
-						b = 0;
-						break;
-					case 2:
-						r = 0;
-						g = 1;
-						b = f;
-						break;
-					case 3:
-						r = 0;
-						g = q;
-						b = 1;
-						break;
-					case 4:
-						r = f;
-						g = 0;
-						b = 1;
-						break;
-					case 5:
-						r = 1;
-						g = 0;
-						b = q;
-						break;
-				}
-				var c = "#" + ("00" + (~~(r * 255)).toString(16)).slice(-2) + ("00" + (~~(g * 255)).toString(16)).slice(-2) + ("00" + (~~(b * 255)).toString(16)).slice(-2);
-				return (c);
+				var colors = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#3F51B5', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFC107', '#FF9800', '#FF5722'];
+				var colIndex = Math.floor(Math.random() * colors.length);
+
+				return colors[colIndex];
+
+
 			}
 		}
 	};
